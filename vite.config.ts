95dd7cc51f4fad8c0d40bd16251cb5c9c03c8b86
Timotei Albu <timotei.albu@gmail.com>
2025-07-27 10:31:54 -0600

    disable spa mode - had this enabled when I couldnt get the custom colors to work

--- vite.config.ts
+++ vite.config.ts
@@ -14,9 +14,6 @@ export default defineConfig({
     }),
     tanstackStart({
       customViteReactPlugin: true,
-      spa: {
-        enabled: true,
-      },
     }),
     viteReact(),
     tailwindcss(),
