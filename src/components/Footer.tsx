8db48d666d185a5cb184b087fef8e8e5d3dfe6cc
Timotei Albu <timotei.albu@gmail.com>
2025-07-27 10:31:07 -0600

    update contact information

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -96,7 +96,7 @@ export function Footer() {
                 >
                   <path d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z" />
                 </svg>
-                (555) 123-4567
+                (403) 464-0275
               </p>
               <p>
                 <svg
@@ -110,7 +110,7 @@ export function Footer() {
                 >
                   <path d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z" />
                 </svg>
-                contact@eliashomes.com
+                contact@eliashomesltd.com
               </p>
               <p>
                 <svg
@@ -125,9 +125,7 @@ export function Footer() {
                   <path d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z" />
                   <path d="M15 11a3 3 0 11-6 0 3 3 0 016 0z" />
                 </svg>
-                123 Main Street, Suite 100
-                <br />
-                Your City, ST 12345
+                Calgary, AB
               </p>
             </div>
           </div>
